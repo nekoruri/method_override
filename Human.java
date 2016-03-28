17e0e7032e58bc2a2d91df78277636c4665d637b
NAKAYAMA Masahiro <masa@chichibu.local>
2016-03-28 18:01:17 +0900

    comment

--- Human.java
+++ Human.java
@@ -1,14 +1,26 @@
 public interface Human {
-    String getName();
+    /**
+     * 名前を返す。
+     */
+    default String getName();
 
+    /**
+     * 生まれ年を返す。
+     */
     default public int getBirthyear() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * 自己紹介を返す。
+     */
     default String introduce(int currentYear) {
         return getAge(currentYear) + "歳の" + getName() + "です。";
     }
 
+    /**
+     * 年齢を返す。
+     */
     default int getAge(int currentYear) {
         return currentYear - this.getBirthyear();
     }
--- Only17.java
+++ Only17.java
@@ -1,3 +1,6 @@
+/**
+ * 17歳教の人類はこのインターフェースをmixinする
+ */
 public interface Only17 extends Human {
     default int getAge(int currentYear) {
         return 17;
